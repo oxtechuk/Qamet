288bdee7d64ef4b3b5c986a2cda3c8201ed6e077
Ox tech <oxtech.uk@gmail.com>
2026-08-06 20:23:10 +0200

    f

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+    <title>Qemt Najd</title>
+</head>
+<body>
+    <div id="root"></div>
+</body>
+</html>
