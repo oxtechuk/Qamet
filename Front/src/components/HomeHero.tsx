69ebd3f43c887c25e5bc141a1671ba6a2d01d32a
Ox tech <oxtech.uk@gmail.com>
2026-08-09 14:01:52 +0200

    s

--- Front/src/components/HomeHero.tsx
+++ Front/src/components/HomeHero.tsx
@@ -49,7 +49,7 @@ const HERO_ANIMATIONS = `
 `;
 
 function getYoutubeId(url?: string): string | null {
-    if (!url) return null;
+    if (!url || typeof url !== 'string') return null;
     const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
