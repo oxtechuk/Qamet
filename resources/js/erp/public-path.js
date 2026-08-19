7c08aa26495b8b3171a5ddc2f85ce8580612f6ae
Ox tech <oxtech.uk@gmail.com>
2026-08-19 18:46:12 +0200

    refactor(frontend): clean root dependencies, eliminate legacy Vue ERP and Mix assets, and streamline React SPA

--- resources/js/erp/public-path.js
+++
@@ -1,4 +0,0 @@
-// This file handles dynamic public path setting for Webpack lazy loading chunks in Vue router
-if (window.ERP_BASE_URL) {
-    __webpack_public_path__ = window.ERP_BASE_URL + '/';
-}
